8d8a43f9862ec8ae7999d6e4e98b5045dc7906d1
Karl Swedberg <kswedberg@gmail.com>
2019-09-01 16:19:30 -0400

    Config stuff

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-	presets: ['@babel/preset-env'],
-	plugins: ['@babel/plugin-transform-runtime'],
-	comments: false
-}
+  presets: [
+    '@babel/preset-env',
+    {
+      useBuiltIns: false,
+      modules: false,
+    },
+  ],
+  comments: false,
+};
